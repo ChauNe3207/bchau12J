1a48d089e4f498d41b3feecb796b4f4b9c3840b6
ChauNe3207 <11tranbaochau10j@gmail.com>
2025-02-28 14:05:00 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function login()
 {
   let u=username.value
   let p=pw.value
-  if(u=="baochau" && p=="3207")
+  if(u == "baochau" && p == "3207")
   {
     window.open("home.html")
     document.getElementById(compiler).style.display="block"
